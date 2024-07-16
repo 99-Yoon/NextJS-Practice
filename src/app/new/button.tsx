b992d4b976605b98f1e752e78a1c8f855761ce58
Jiwon <angelayoon99@gmail.com>
2024-07-16 14:02:47 +0900

    butoon storybook

--- src/app/new/button.tsx
+++ src/app/new/button.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { Button } from "@/stories/Button";
+
 export default function NewButton() {
   const newButtonClick = () => {
     console.log("새로운 우언가를 찾아보아요");
   };
-  return <button onClick={newButtonClick}>새로운 버튼</button>;
+  return <div></div>;
 }
--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/stories/Button";
 import NewButton from "./button";
 
 export default function New() {
   return (
     <div>
       <h1>새로운 페이지입니다. </h1>
-      <NewButton />
+      <Button label={"버튼입니다"} primary backgroundColor="#000000" />
     </div>
   );
 }
